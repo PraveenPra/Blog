6f79fc80ba648405841aa14534e39ca17b520961
PraveenPra <prvp6501@gmail.com>
2024-06-20 01:57:19 +0530

    Added more options to quill editor toolbar

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -43,19 +43,30 @@
         </form>
     </div>
 
-    <script src="https://cdn.quilljs.com/1.3.6/quill.min.js" defer></script>
+    <!-- <script src="https://cdn.quilljs.com/1.3.6/quill.min.js" defer></script> -->
     <script>
         var editor = new Quill('#editor', {
             theme: 'snow',
             modules: {
                 toolbar: [
-                    [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
-                    [{size: []}],
-                    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-                    [{'list': 'ordered'}, {'list': 'bullet'}, 
-                     {'indent': '-1'}, {'indent': '+1'}],
-                    ['link', 'image', 'video'],
-                    ['clean']                                         
+                    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+  ['link', 'image', 'video', 'formula'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button                                        
                 ]
             }
         });
