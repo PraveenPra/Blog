e6e1609624624ca82950bbcd305dd1f6ce0deda5
PraveenPra <prvp6501@gmail.com>
2024-06-19 17:05:10 +0530

    Defined relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'body', 'category_id', 'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
