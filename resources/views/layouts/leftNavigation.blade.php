67b6e96a84d9e4f069e3b43fb13c29a2b1704cc6
PraveenPra <prvp6501@gmail.com>
2024-07-01 19:38:29 +0530

    Icons for left nav complete

--- resources/views/layouts/leftNavigation.blade.php
+++ resources/views/layouts/leftNavigation.blade.php
@@ -16,56 +16,51 @@
 
 
             <div class="flex flex-col items-end gap-4">
-
-
-
                 @can('view categories')
-                <div class="shrink-0 flex items-center border-b-2 border-gray-50  p-4 sm:p-6 lg:p-8">
-                    <a href="{{ route('categories.index') }}" class="shrink-0 flex items-center gap-2 {{request()->routeIs('categories.index')? 'border-r-4 border-gray-500' : ''}}">
+                <div class="group shrink-0 flex items-center border-b-2 border-gray-50 p-4 sm:p-6 lg:p-8  
+                {{request()->routeIs('categories.index')? 'border-r-4 ' : ''}}">
+                    <a href="{{ route('categories.index') }}" class="shrink-0 flex items-center gap-2 ">
                         <i class="fa-solid fa-layer-group" style="font-size: 20px;"></i>
-                        Categories
+                        <span class="hidden group-hover:inline-block">Categories</span>
                     </a>
                 </div>
                 @endcan
 
-
                 @can('view tags')
-                <div class="shrink-0 flex items-center border-b-2 border-gray-50  p-4 sm:p-6 lg:p-8">
+                <div class="group shrink-0 flex items-center border-b-2 border-gray-50 p-4 sm:p-6 lg:p-8">
                     <a href="{{ route('tags.index') }}" class="shrink-0 flex items-center gap-2 {{request()->routeIs('tags.index')? 'border-r-4 border-gray-500' : ''}}">
                         <i class="fa-solid fa-hashtag" style="font-size: 20px;"></i>
-                        Tags
+                        <span class="hidden group-hover:inline-block">Tags</span>
                     </a>
                 </div>
                 @endcan
 
                 @can('manage users')
-                <div class="shrink-0 flex items-center border-b-2 border-gray-50  p-4 sm:p-6 lg:p-8">
+                <div class="group shrink-0 flex items-center border-b-2 border-gray-50 p-4 sm:p-6 lg:p-8">
                     <a href="{{ route('users.index') }}" class="shrink-0 flex items-center gap-2 {{request()->routeIs('users.index')? 'border-r-4 border-gray-500' : ''}}">
                         <i class="fa-solid fa-users" style="font-size: 20px;"></i>
-                        Users
+                        <span class="hidden group-hover:inline-block">Users</span>
                     </a>
                 </div>
                 @endcan
 
-
                 @can('view roles')
-                <div class="shrink-0 flex items-center border-b-2 border-gray-50  p-4 sm:p-6 lg:p-8">
+                <div class="group shrink-0 flex items-center border-b-2 border-gray-50 p-4 sm:p-6 lg:p-8">
                     <a href="{{ route('roles.index') }}" class="shrink-0 flex items-center gap-2 {{request()->routeIs('roles.index')? 'border-r-4 border-gray-500' : ''}}">
                         <i class="fa-solid fa-people-roof" style="font-size: 20px;"></i>
-                        Roles
+                        <span class="hidden group-hover:inline-block">Roles</span>
                     </a>
                 </div>
                 @endcan
 
                 @can('view permissions')
-                <div class="shrink-0 flex items-center   p-4 sm:p-6 lg:p-8">
+                <div class="group shrink-0 flex items-center p-4 sm:p-6 lg:p-8">
                     <a href="{{ route('permissions.index') }}" class="shrink-0 flex items-center gap-2 {{request()->routeIs('permissions.index')? 'border-r-4 border-gray-500' : ''}}">
                         <i class="fa-solid fa-check-double" style="font-size: 20px;"></i>
-                        Permissions
+                        <span class="hidden group-hover:inline-block">Permissions</span>
                     </a>
                 </div>
                 @endcan
-
 
                 <!-- Navigation Links (Initially Hidden on Small Screens) -->
                 <div class="hidden space-y-2 sm:flex sm:flex-col sm:items-start sm:ms-10">
@@ -73,6 +68,7 @@
                     <!-- Include all your links here as shown in your original code -->
                 </div>
             </div>
+
 
             <!-- Settings Dropdown and Hamburger Icon -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
